bda9a14966d0a95f4c419ca0035777a77b3d7983
Tim Wieder <tim.wieder@plentymarkets.com>
2019-06-12 16:02:41 +0200

    - added pipe to display a video

--- src/app/components/directive-collection.ts
+++ src/app/components/directive-collection.ts
@@ -5,6 +5,7 @@ import { FixedHeaderDirective } from './tables/fixed-header/fixed-header.directi
 import { TerraLabelTooltipDirective } from '../helpers/terra-label-tooltip.directive';
 import { FormEntryContainerDirective } from './forms/form/form-entry/form-entry-container.directive';
 import { CKEditorFullDirective } from './editors/ck-editor/ck-editor-full.directive';
+import { EscapeHtmlPipe } from './editors/ck-editor/example/ck-editor-full.directive.example';
 
 export const directives:Array<Type<any>> = [
     TerraDataTableContextMenuDirective,
@@ -12,5 +13,6 @@ export const directives:Array<Type<any>> = [
     FixedHeaderDirective,
     TerraLabelTooltipDirective,
     FormEntryContainerDirective,
+    EscapeHtmlPipe,
     CKEditorFullDirective
 ];
--- src/app/components/editors/ck-editor/example/ck-editor-full.directive.example.ts
+++ src/app/components/editors/ck-editor/example/ck-editor-full.directive.example.ts
@@ -3,6 +3,11 @@ import {
     ckEditorFullConfig,
     ckEditorMinimumConfig
 } from '../presets/ck-editor-presets';
+import {
+    DomSanitizer,
+    SafeHtml
+} from '@angular/platform-browser';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
     selector: 'tc-full-directive-example',
@@ -12,4 +17,22 @@ export class CkEditorFullDirectiveExample
 {
     protected config:{} = ckEditorFullConfig;
     protected tiny:{} = ckEditorMinimumConfig;
+
+    protected html:any;
+
+}
+
+
+
+@Pipe({ name: 'keepHtml', pure: false })
+export class EscapeHtmlPipe implements PipeTransform
+{
+    constructor(private sanitizer:DomSanitizer)
+    {
+    }
+
+    public transform(content:string):SafeHtml
+    {
+        return this.sanitizer.bypassSecurityTrustHtml(content);
+    }
 }

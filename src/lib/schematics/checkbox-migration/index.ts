c81322164d6e6b9d762288a812ea01e5a55c49f8
Daniel Trauf <daniel.trauf@plentymarkets.com>
2020-06-26 15:29:14 +0200

    add parse helper

--- src/lib/schematics/checkbox-migration/index.ts
+++ src/lib/schematics/checkbox-migration/index.ts
@@ -9,10 +9,15 @@ import { LoggerApi } from '@angular-devkit/core/src/logger';
 import { StringReplacementInterface } from '../string-replacement.interface';
 
 const componentPath:string = './src/app/app.component.html';
-// const componentPathTest:string = 'test.component.ts';
+
+// const querySelectorStart:string = '`<terra-selectbox>`';
+// const querySelectorEnd:string = '`</terra-selectbox>`';
 
 const stringsToReplace:Array<StringReplacementInterface> = [
-    {query: '[inputIsDisabled]', replacement: '[disabled]'}
+    {
+        query:       '[inputIsDisabled]',
+        replacement: '[disabled]'
+    }
 ];
 
 // You don't have to export the function as default. You can also have more than one rule factory
@@ -28,8 +33,6 @@ export function checkboxMigration(_options:any):Rule
 async function runMigration(tree:Tree, context:SchematicContext)
 {
     const logger:LoggerApi = context.logger;
-    // tree.create(componentPathTest, 'console.log("Foobar!")');
-
     logger.info(`Content before update:\n${tree.read(componentPath)} `);
 
     if(tree.exists(componentPath))
@@ -39,7 +42,7 @@ async function runMigration(tree:Tree, context:SchematicContext)
 
         stringsToReplace.forEach((query:StringReplacementInterface) =>
         {
-            update = replaceTemplateProperties(update, buffer, query.replacement, query.query);
+            replaceTemplateProperties(update, buffer, query.replacement, query.query);
         });
         tree.commitUpdate(update);
     }
@@ -47,10 +50,9 @@ async function runMigration(tree:Tree, context:SchematicContext)
     logger.info(`Content after update:\n${tree.read(componentPath)}`);
 }
 
-function replaceTemplateProperties(update:UpdateRecorder, buffer:Buffer | number, replaceString:string, queryString:string):UpdateRecorder
+function replaceTemplateProperties(update:UpdateRecorder, buffer:Buffer | number, replaceString:string, queryString:string)
 {
     const startIndex:number = buffer.toString().indexOf(queryString);
     update.insertRight(startIndex, replaceString);
     update.remove(startIndex, queryString.length);
-    return update;
 }
